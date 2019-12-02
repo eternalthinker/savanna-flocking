059d3aaacd226e2f95cdb069736f5aa016814f5f
Rahul Anand <rahulanand.inbox@gmail.com>
2019-12-03 01:35:15 +1100

    Spawn antelopes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Drawable } from "./Drawable";
 import { Lion } from "./Lion";
 import "./index.css";
 import antelopeSheet from "./assets/antelope_sheet.png";
@@ -5,6 +6,8 @@ import lionSheet from "./assets/lion_sheet.png";
 import { Antelope } from "./Antelope";
 import { Animation } from "./Sprite";
 import { Game } from "./Game";
+import { Perishable } from "./Perishable";
+import { Vector } from "./Vector";
 
 function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement) {
   // look up the size the canvas is being displayed
@@ -43,6 +46,19 @@ function initCanvas() {
   game.canvas = canvas;
 }
 
+function initBgCanvas(game: Game) {
+  const canvas = <HTMLCanvasElement>document.getElementById("bgCnvs");
+  resizeCanvasToDisplaySize(canvas);
+
+  const ctx = canvas.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
+  ctx.translate(0.5, 0.5);
+
+  ctx.fillStyle = "#70b575";
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  ctx.fill();
+}
+
 function getFlipXSpriteSheet(animation: Animation) {
   const spriteSheet = animation.spriteSheet;
   const flipCanvas = document.createElement("canvas");
@@ -77,12 +93,40 @@ function getFlipXSpriteSheet(animation: Animation) {
   return animCanvas;
 }
 
+class CircleDrawable implements Drawable {
+  private size: number;
+  private color = {
+    r: 255,
+    g: 255,
+    b: 255
+  };
+
+  constructor(private game: Game) {
+    const antelopeSheet = this.game.sprites.antelopeSheet;
+    this.size = antelopeSheet.height / 2;
+  }
+
+  draw(x: number, y: number, opacity?: number) {
+    const ctx = this.game.ctx;
+
+    const alpha = opacity == null ? 1 : opacity;
+    const c = this.color;
+
+    ctx.strokeStyle = null;
+    ctx.fillStyle = `rgba(${c.r}, ${c.g}, ${c.b}, ${alpha})`;
+    ctx.beginPath();
+    ctx.arc(x, y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
+
+let lastUpdateTime = Date.now();
+let accTimeDelta = 0;
+
 function draw(antelopes: Antelope[], lions: Lion[]) {
   const game = Game.getInstance();
   const ctx = game.ctx;
-  // ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
-  ctx.fillStyle = "#987ffa";
-  ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
+  ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
 
   game.layers["background"].update();
 
@@ -99,6 +143,27 @@ function draw(antelopes: Antelope[], lions: Lion[]) {
   });
 
   game.layers["foreground"].update();
+
+  const currentTime = Date.now();
+  const timeDelta = currentTime - lastUpdateTime;
+  accTimeDelta += timeDelta;
+  lastUpdateTime = currentTime;
+
+  if (accTimeDelta > 5000) {
+    accTimeDelta = 0;
+    const x = Math.floor(Math.random() * game.canvas.width);
+    const y = Math.floor(Math.random() * game.canvas.height);
+    const antelope = new Antelope(x, y, game);
+    antelopes.push(antelope);
+    antelope.update();
+    const _perishable = new Perishable(
+      game,
+      new CircleDrawable(game),
+      new Vector(x, y),
+      100,
+      0.1
+    );
+  }
 
   window.requestAnimationFrame(() => draw(antelopes, lions));
 }
@@ -124,6 +189,8 @@ Promise.all([loadImage(antelopeSheet), loadImage(lionSheet)]).then(
       })
     };
 
+    initBgCanvas(game);
+
     const antelopes: Antelope[] = [];
     for (let i = 0; i < 50; ++i) {
       const x = Math.floor(Math.random() * game.canvas.width);
@@ -132,7 +199,7 @@ Promise.all([loadImage(antelopeSheet), loadImage(lionSheet)]).then(
     }
 
     const lions: Lion[] = [];
-    for (let i = 0; i < 20; ++i) {
+    for (let i = 0; i < 10; ++i) {
       const x = Math.floor(Math.random() * game.canvas.width);
       const y = Math.floor(Math.random() * game.canvas.height);
       lions.push(new Lion(x, y, game));
